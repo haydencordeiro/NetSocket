0c2efbd71564e46864c7b9a6fbf6729f37aba8b8
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-03-04 22:08:36 -0500

    boilerplate code for socket sever and client

---
+++ client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[BUFFER_SIZE] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    while(1) {
+        // Send message to server
+        send(sock, hello, strlen(hello), 0);
+        printf("Client: %s\n", hello);
+        // Read message from server
+        valread = read(sock, buffer, BUFFER_SIZE);
+        if (valread == 0) {
+            printf("Server disconnected\n");
+            break;
+        }
+        printf("Server: %s\n",buffer);
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+    close(sock);
+    return 0;
+}
